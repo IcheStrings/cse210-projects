8d97c88542070035c21be1f85ee9fcb54ca55b50
IcheStrings <icheenwuka@gmail.com>
2024-09-29 16:48:02 +0100

    interating

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); // Generate a random number from 1 to 100
+
+        int userGuess = 0;
+        
+        Console.WriteLine("I've picked a number between 1 and 100. Try to guess it!");
+
+        while (userGuess != magicNumber)
+        {
+            Console.Write("Enter your guess: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                userGuess = int.Parse(input);
+
+                if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Go higher.");
+                }
+                else if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Go lower.");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed it right.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
     }
 }
